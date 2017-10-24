69352e32f6c095d0f7831b7cd3e1ceb8d55d64ac
Dan Karlin <dlkventuresllc@gmail.com>
2017-10-24 00:00:03 +0000

    added episode controller and views

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,9 +1,10 @@
 class PodcastsController < ApplicationController
-def index
-    @podcasts = Podcast.all.order("created_at DESC")
-end
+	def index
+		@podcasts = Podcast.all.order("created_at DESC")
+	end
 
-def show
-    @podcast = Podcast.find(params[:id])
-end
+	def show
+		@podcast = Podcast.find(params[:id])
+		@episodes = Episode.where(podcast_id: @podcast).order("created_at DESC")
+	end
 end
