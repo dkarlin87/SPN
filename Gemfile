13ee27d14cf5e63eac0fd1dcb25cbb416dd69196
Dan Karlin <dlkventuresllc@gmail.com>
2017-10-25 23:17:59 +0000

    added paperclip image uploading to podcasts and episodes

--- Gemfile
+++ Gemfile
@@ -12,7 +12,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise', '~> 4.3'
-
+gem 'paperclip', '~> 5.1'
 # gem 'bcrypt', '~> 3.1.7'
 
 # gem 'unicorn'
--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -40,7 +40,7 @@ class EpisodesController < ApplicationController
 	private
 
 	def episode_params
-		params.require(:episode).permit(:title, :description)
+		params.require(:episode).permit(:title, :description, :thumbnail)
 	end
 
 	def find_podcast
@@ -51,11 +51,11 @@ class EpisodesController < ApplicationController
 		@episode = Episode.find(params[:id])
 	end
 
-def require_permission
-@podcast = Podcast.find(params[:podcast_id])
-if current_podcast != @podcast
-	redirect_to root_path, notice: "Sorry, you're not allowed to view that page!"
-end
+	def require_permission
+		@podcast = Podcast.find(params[:podcast_id])
+			if current_podcast != @podcast
+				redirect_to root_path, notice: "Sorry, you're not allowed to view that page!"
+	end
 end
 
 end
